783587f7dec937ce80273ec83c28e3078b32ec18
Lina Hall <liina00girl@hotmail.com>
2026-06-05 17:13:48 +0200

    Added Game and GenreDTO

---
+++ Backend/GameStoreApp.Application/Games/GameDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStoreApp.Application.Games
+{
+    public class GameDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string Description { get; set; }= string.Empty;
+        public int ReleaseYear { get; set; }
+        //API return this Genrename and not the whole entity of genre
+        public string GenreName { get; set; } = string.Empty;
+    }
+}
