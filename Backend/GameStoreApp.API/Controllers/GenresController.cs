1b5ece68c4864dd81e118fa203930d5f2626c0e0
Lina Hall <liina00girl@hotmail.com>
2026-06-08 14:39:06 +0200

    Done with API controllers for both Games and Genres, registrated in program.cs, tested all endpoints in swagger without all working atm

---
+++ Backend/GameStoreApp.API/Controllers/GenresController.cs
@@ -0,0 +1,49 @@
+﻿using GameStoreApp.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+using GameStoreApp.Application.DTOs;
+
+namespace GameStoreApp.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class GenresController : ControllerBase
+{
+    private readonly GenreService _genreService;
+    public GenresController(GenreService genreService)//injecting GenreService
+    {
+        _genreService = genreService;
+    }
+    [HttpGet]//get genres
+    public async Task<ActionResult<IEnumerable<GenreDto>>> GetAll()
+    {
+        var genres = await _genreService.GetAllAsync();
+        return Ok(genres);
+    }
+    [HttpGet("{id}")]//get by id
+    public async Task<ActionResult<GenreDto>> GetById(int id)
+    {
+        var genre = await _genreService.GetByIdAsync(id);
+        if (genre == null)
+            return NotFound();
+
+        return Ok(genre);
+    }
+    [HttpPost]
+    public async Task<ActionResult> Create(GenreDto dto)
+    {
+        await _genreService.AddAsync(dto);
+        return Ok();
+    }
+    [HttpPut("{id}")]//updates
+    public async Task<ActionResult> Update(int id, GenreDto dto)
+    {
+        await _genreService.UpdateAsync(id, dto);
+        return NoContent();
+    }
+    [HttpDelete("{id}")]//DELETE
+    public async Task<ActionResult> Delete(int id)
+    {
+        await _genreService.DeleteAsync(id);
+        return NoContent();
+    }
+}
