380abdd672e4e0c4de5d896b2bab469ca3c8a161
Lina Hall <liina00girl@hotmail.com>
2026-06-08 12:43:35 +0200

    Added, GameService and GenreService with business logic, and then I Added ReleaseYear to Game entity since i missed it before

---
+++ Backend/GameStoreApp.Application/Services/GameService.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using GameStoreApp.Domain.Entities;
+using GameStoreApp.Application.DTOs;
+using GameStoreApp.Application.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace GameStoreApp.Application.Services
+{
+    public class GameService
+    {
+        private readonly IGameRepository _gameRepository;
+        private readonly IGenreRepository _genreRepository;
+
+        public GameService(IGameRepository gameRepository, IGenreRepository genreRepository)
+        {
+            _gameRepository = gameRepository;
+            _genreRepository = genreRepository;
+        }
+        public async Task<IEnumerable<GameDto>> GetAllAsync()//Gets all games
+        {
+            var games = await _gameRepository.GetAllAsync();
+
+            return games.Select(g => new GameDto
+            {
+                Id = g.Id,
+                Title = g.Title,
+                Price = g.Price,
+                Description = g.Description,
+                ReleaseYear = g.ReleaseYear,
+                GenreName = g.Genre?.Name ?? ""
+            });
+        }
+        public async Task<GameDto?> GetByIdAsync(int id)//Gets game by its ID, maps to DTO 
+        {
+            var game = await _gameRepository.GetByIdAsync(id);
+            if (game == null)
+                return null;
+            return new GameDto
+            {
+                Id = game.Id,
+                Title = game.Title,
+                Price = game.Price,
+                Description = game.Description,
+                ReleaseYear = game.ReleaseYear,
+                GenreName = game.Genre?.Name ?? ""
+            };
+        }
+        public async Task AddAsync(GameDto dto)//create new GAme
+        {
+            var genre = await _genreRepository.GetByIdAsync(dto.Id);
+            if (genre == null)
+                throw new Exception("Genre not found...");
+
+            var game = new Game
+            {
+                Title = dto.Title,
+                Price = dto.Price,
+                Description = dto.Description,
+                ReleaseYear = dto.ReleaseYear,
+                GenreId = genre.Id
+            };
+            await _gameRepository.AddAsync(game);
+        }
+        public async Task UpdateAsync(int id, GameDto dto) //updates a GAME
+        {
+            var game = await _gameRepository.GetByIdAsync(id);
+            if (game == null)
+                throw new Exception("Game not found..");
+            game.Title = dto.Title;
+            game.Price = dto.Price;
+            game.Description = dto.Description;
+            game.ReleaseYear = dto.ReleaseYear;
+
+            await _gameRepository.UpdateAsync(game);
+        }
+        public async Task DeleteAsync(int id)//delets a GAME
+        {
+            await _gameRepository.DeleteASync(id);
+        }
+    }
+}
---
+++ Backend/GameStoreApp.Application/Services/GenreService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using GameStoreApp.Domain.Entities;
+using GameStoreApp.Application.DTOs;
+using GameStoreApp.Application.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStoreApp.Application.Services
+{
+    public class GenreService
+    {
+        private readonly IGenreRepository _genreRepository;
+        public GenreService(IGenreRepository genreRepository)
+        {
+            _genreRepository = genreRepository;
+        }
+        public async Task<IEnumerable<GenreDto>> GetAllAsync()//gets all genres, maps to dtop
+        {
+            var genres = await _genreRepository.GetAllAsync();
+            return genres.Select(g => new GenreDto
+            {
+                Id = g.Id,
+                Name = g.Name,
+            });
+        }
+        public async Task<GenreDto?> GetByIdAsync(int id)//gets "a" genre by its ID
+        {
+            var genre = await _genreRepository.GetByIdAsync(id);
+            if (genre == null)
+                return null;
+            return new GenreDto
+            {
+                Id = genre.Id,
+                Name = genre.Name
+            };
+        }
+        public async Task AddAsync(GenreDto dto)//create new GENRE
+        {
+            var genre = new Genre
+            {
+                Name = dto.Name
+            };
+            await _genreRepository.AddAsync(genre);
+        }
+        public async Task UpdateAsync(int id, GenreDto dto)//updates a genre
+        {
+            var genre = await _genreRepository.GetByIdAsync(id);
+            if (genre == null)
+                throw new Exception("Genre not found...");
+
+            genre.Name = dto.Name;
+            await _genreRepository.UpdateAsync(genre);
+        }
+        public async Task DeleteAsync(int id)//deletes a genre
+        {
+            await _genreRepository.DeleteAsync(id);
+        }
+    }
+}
